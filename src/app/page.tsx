559515300dbda82c8c0c1d32da97a4739e67d871
NJ9802 <nelsonjavier9802@gmail.com>
2023-09-26 12:26:35 -0400

    app: Add App Presenter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <main>Exchange Calculator</main>;
+import Service from "@/core/services/exchangeServices";
+import ServiceInteractor from "@/core/useCases/serviceInteractor";
+import AppController from "@/infrastructure/controllers/AppController";
+
+import "material-symbols/outlined.css";
+
+export default async function AppPresenter() {
+  const service = new Service();
+  const serviceInteractor = new ServiceInteractor(service);
+
+  const currencies = await serviceInteractor.getCurrencies();
+
+  return (
+    <main>
+      <AppController currencies={currencies} />
+    </main>
+  );
 }
